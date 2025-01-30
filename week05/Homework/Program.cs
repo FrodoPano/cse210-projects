d34bdd445de5eaa152323d500ccb9c4ddd0f23c9
FrodoPano <frodopano@gmail.com>
2025-01-30 09:33:10 -0600

    Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment first = new Assignment("Brock Samson","Multiplication");
+        Console.WriteLine(first.GetSummary());
+
+        MathAssignment second = new MathAssignment("Dean Venture", "Pi", "8", "12-14");
+        Console.WriteLine(second.GetSummary());
+        Console.WriteLine(second.GetHomeworkList());
+
+        WritingAssignment third = new WritingAssignment("Gary Henchman", "Fantasy", "Character Development");
+        Console.WriteLine(third.GetSummary());
+        Console.WriteLine(third.GetWritingInformation());
     }
 }

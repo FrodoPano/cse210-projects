0898795392626e354f082161bdd26afb63351fd7
FrodoPano <frodopano@gmail.com>
2025-02-01 10:52:31 -0600

    Mindfulness Design

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,25 @@
+public class Activity{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(){
+
+    }
+
+    public void DisplayStartingMessage(){
+
+    }
+
+    public void DiplayEndingMessage(){
+
+    }
+
+    public void ShowSpinner(int seconds){
+
+    }
+
+    public void ShowCountDown(int seconds){
+
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+public class ReflectingActivity : Activity{
+
+    private List<string> _prompts = new List<string>(){
+
+    };
+
+    private List<string> _questions = new List<string>(){
+
+    };
+
+    public ReflectingActivity(){
+
+    }
+
+    public void Run(){
+
+    }
+
+    public string GetRandomPrompt(){
+        return null;
+    }
+
+    public string GetRandomQuestion(){
+        return null;
+    }
+
+    public void DisplayPrompt(){
+
+    }
+
+    public void DisplayQuestions(){
+        
+    }
+
+}

906bccee609083302a5bf92ed0c34aa943727130
FrodoPano <frodopano@gmail.com>
2025-02-06 09:44:06 -0600

    Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square form1 = new Square("Green", 3);
+        shapes.Add(form1);
+
+        Rectangle form2 = new Rectangle("White", 3, 5);
+        shapes.Add(form2);
+
+        Circle form3 = new Circle("Orange", 7);
+        shapes.Add(form3);
+
+        foreach(Shape i in shapes){
+            string color = i.GetColor();
+
+            double area = i.GetArea();
+
+            Console.WriteLine($"The color {color} is in the area of {area}");
+        }
+        
     }
 }
